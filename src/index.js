4fa7d624f7ac1b528beb80175d5c7a7ec287a142
kamikeeen <unjo.kamiken@gmail.com>
2019-11-21 17:49:18 +0900

    react-sample

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { PersistGate } from 'redux-persist/integration/react'
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import MemoStore, { memoReducer } from './memo/Store'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const persistConfig = {
+  key: 'memo',
+  storage: storage,
+  blacklist: ['message', 'mode', 'fdata'],
+  whitelist: ['data']
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const persistedReducer = persistReducer(persistConfig, memoReducer)
+
+let store = createStore(persistedReducer)
+let pstore = persistStore(store)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate loading={<p>loading...</p>} persistor={pstore}>
+      <App />
+    </PersistGate>
+  </Provider>,
+  document.getElementById('root')
+)
+
+export default pstore
